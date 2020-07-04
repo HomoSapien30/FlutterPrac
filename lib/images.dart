f92f9fb6e336755deaa9aad09cc4324c53f91714
Dhruv Goyal <44157532+dhruvgoyal12@users.noreply.github.com>
2020-07-04 22:22:18 +0530

    updating images.dart with link functionality

--- lib/images.dart
+++ lib/images.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Images extends StatefulWidget {
   Images() : super();
@@ -8,9 +9,12 @@ class Images extends StatefulWidget {
 }
 
 class _ImagesState extends State<Images> {
+  List<Color> textColor = [Colors.black, Colors.white];
+  List<Color> buttonColor = [Colors.white, Colors.black];
   CarouselSlider carouselSlider;
-  int _current=0;
-  List imgList=[
+  int _current = 0;
+  int current = 0;
+  List imgList = [
     'assets/download.jpg',
     'assets/download.jpg',
     'assets/download.jpg',
@@ -22,7 +26,7 @@ class _ImagesState extends State<Images> {
     'assets/download.jpg',
     'assets/download.jpg',
     'assets/download.jpg',
-    'assets/download.jpg'
+    'assets/surprise.jpg',
   ];
   List<T> map<T>(List list, Function handler) {
     List<T> result = [];
@@ -30,6 +34,22 @@ class _ImagesState extends State<Images> {
       result.add(handler(i, list[i]));
     }
     return result;
+  }
+
+  colorChanger(current) {
+    setState(() {
+      current = current + 1;
+    });
+  }
+
+  zoom(String imgUrl) async {
+    const url = "https://flutter.dev";
+    if (imgUrl == 'assets/surprise.jpg') {
+      print("hello");
+      if (await canLaunch(url)) {
+        await launch(url);
+      }
+    }
   }
 
   @override
@@ -40,45 +60,44 @@ class _ImagesState extends State<Images> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            carouselSlider=CarouselSlider(
+            carouselSlider = CarouselSlider(
               options: CarouselOptions(
                 height: 500,
                 initialPage: 0,
                 enlargeCenterPage: true,
                 autoPlay: true,
                 reverse: false,
-                enableInfiniteScroll: true,
+                enableInfiniteScroll: false,
                 autoPlayInterval: Duration(seconds: 2),
                 autoPlayAnimationDuration: Duration(milliseconds: 2000),
                 scrollDirection: Axis.horizontal,
                 onPageChanged: callbackfunction(_current),
-
               ),
               items: imgList.map((imgUrl) {
                 return Builder(
                   builder: (BuildContext context) {
+                    //print(imgUrl);
                     return Container(
+                      child: GestureDetector(
+                        onTap: () => zoom(imgUrl),
+                      ),
                       width: MediaQuery.of(context).size.width,
                       margin: EdgeInsets.symmetric(horizontal: 10.0),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(5),
                           color: Colors.green,
-                          image:DecorationImage(
-                          image: AssetImage(imgUrl),
-                          fit: BoxFit.cover
-                        )
-                      ),
-
+                          image: DecorationImage(
+                              image: AssetImage(imgUrl), fit: BoxFit.cover)),
                     );
                   },
                 );
               }).toList(),
-
-            )
+            ),
+            
           ],
         ),
       ),
-    ) ;
+    );
   }
 
   callbackfunction(int index) {
@@ -86,6 +105,7 @@ class _ImagesState extends State<Images> {
       setState(() {
         _current = index;
       });
-    };
+    }
+    ;
   }
 }
